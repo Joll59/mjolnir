4ad7c30516ffd78bff44f7b24956c38b4d87d65e
Yomi Lajide <v-ablaji@microsoft.com>
2018-02-20 09:00:07 -0800

    moved issues to readme & add some room logic for game

--- src/__tests__/reducers/room.test.ts
+++ src/__tests__/reducers/room.test.ts
@@ -1,29 +1,18 @@
-// // import { expect } from 'chai';
-// import { Room } from '../../types/index';
+// import { expect } from 'chai';
+import { Room } from '../../types/index';
 
-// let roomState: Room[];
-// let nextState: Room[];
-// describe('RoomReducer', () => {
+let roomState: Room[];
 
-//     it('RoomReducer is defined', 
-//        () => {
-//             expect(RoomReducer()).toBeDefined();
-//         });
+describe('RoomReducer', () => {
 
-//     it( 'room state is an array',
-//         () => {
-//             expect(Array.isArray(roomState)).toEqual(true);
-//         }
-//     );
-//     describe('State changes', () => {
-//         it('create room in roomState and returns nextState', 
-//            () => {
-//             nextState = RoomReducer(roomState, createRoom([1, 2]));
-//             expect(nextState).not.toEqual(roomState);
-//         });
-//         it('expect nextState to include newly added room', 
-//            () => {
-//             expect(nextState[nextState.length - 1]).toContain({location: [1, 2]});
-//         });
-//     });
-// });
+    it('RoomReducer is defined', 
+       () => {
+            expect(RoomReducer()).toBeDefined();
+        });
+
+    it( 'room state is an array',
+        () => {
+            expect(Array.isArray(roomState)).toEqual(true);
+        }
+    );
+});
